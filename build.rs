03a9ff11ca8edfaf002068af5fca1d40fab76158
Anton Feldmann <anton.feldmann@toennies-ai.de>
2025-06-16 17:44:44 +0200

    change PYLON_ROOT in build.rs

--- build.rs
+++ build.rs
@@ -33,6 +33,7 @@ fn main() {
     #[cfg(target_os = "linux")]
     {
         let pylon_root = std::env::var("PYLON_ROOT").unwrap_or_else(|_| "/opt/pylon".into());
+        println!("cargo:warning=PYLON_ROOT = {:?}", std::env::var("PYLON_ROOT"));
 
         let expected_major_version = match pylon_root.as_str() {
             "/opt/pylon5" => Some(5),
